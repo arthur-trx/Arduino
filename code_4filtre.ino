ddfd9e4bfa3106884f24d192932ca9380f5cda22
arthur-trx <90566883+arthur-trx@users.noreply.github.com>
2022-04-05 18:06:54 +0200

    Add files via upload

---
+++ code_4filtre.ino
@@ -0,0 +1,24 @@
+#include <Wire.h> 
+#define I2C_Out 0x21
+#define I2C_In 0x24
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("init");
+}
+
+void loop() {
+  Serial.println ("new");
+  Wire.begin() ; 
+  Wire.requestFrom(I2C_In,1); // On lit 1 Octet
+  byte C_I2C = Wire.read() ; 
+  byte C = C_I2C&B00111100;
+  Serial.println(C, BIN);
+  Wire.beginTransmission (I2C_Out) ; // Envoie de l'adresse du périphérique I²C
+  Wire.write(C) ; // Ecriture d’un octet sur la liaison série de type I²C
+  Wire.endTransmission() ; // Fin de transmission sur la liaison série de type I²C
+  delay (5);
+}
