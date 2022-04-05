2de082168a7c9fc197319e350c1fd7a1be86f80e
arthur-trx <90566883+arthur-trx@users.noreply.github.com>
2022-04-05 15:32:09 +0200

    Add files via upload

---
+++ code_4/sketch_mar24b.ino
@@ -0,0 +1,34 @@
+#include <Wire.h> 
+#define I2C_Out 0x21
+#define I2C_In 0x24
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("init");
+}
+
+void loop() {
+  Serial.println ("new");
+  Wire.begin() ; 
+  Wire.requestFrom(I2C_In,1); // On lit 1 Octet
+  byte C_I2C = Wire.read() ; 
+  Serial.println(C_I2C);
+  Wire.beginTransmission (I2C_Out) ; // Envoie de l'adresse du périphérique I²C
+  Wire.write(C_I2C) ; // Ecriture d’un octet sur la liaison série de type I²C
+  Wire.endTransmission() ; // Fin de transmission sur la liaison série de type I²C
+  delay (5);
+  /*Wire.beginTransmission (I2C_Out) ; // Envoie de l'adresse du périphérique I²C
+  Wire.write(0) ; // Ecriture d’un octet sur la liaison série de type I²C
+  Wire.endTransmission() ; // Fin de transmission sur la liaison série de type I²C
+  delay (30);*/
+}
+
+
+/*void Write(byte _data) {
+  Wire.beginTransmission(I2C_Out); // Commence la transmission sur l'adresse défini plus haut
+  Wire.write(_data);              // On envoie la donnée
+  Wire.endTransmission();         // On termine la transmission
+}*/
